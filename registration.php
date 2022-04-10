35eecca08e818783e56e46de1fb57720437484a0
Rodrigo <86023272+SilRodrigo@users.noreply.github.com>
2022-04-10 19:39:29 -0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -4,6 +4,6 @@ use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-    'Tezus_FrameworkBase',
+    'Vendor_ModuleName',
     __DIR__
 );
